b30110655dac208d8a358e4535d637198b5d0e07
AnaModesto21 <77633954+AnaModesto21@users.noreply.github.com>
2022-05-07 13:41:16 +0100

    update

--- backend/app.js
+++ backend/app.js
@@ -31,17 +31,20 @@ app.use(fileUpload());
 
 const errorMiddleware = require('./middlewares/errors')
 // Import all routes
-const products = require("./routes/product");
-const auth = require('./routes/auth');
+// const products = require("./routes/product");
+// const auth = require('./routes/auth');
 const payment = require('./routes/payment');
 const order = require('./routes/order');
 
-app.use('/products', products);
+// app.use('/api', products);
 app.use('/auth', auth)
 app.use('/payment', payment)
 app.use('/order', order)
 
-
+const productRoutes = require("./routes/product");
+app.use("/api", productRoutes);
+const authRoutes = require ("./routes/auth");
+app.use("/api", authRoutes);
 
 
 // const allRoutes = require("./routes/order");
