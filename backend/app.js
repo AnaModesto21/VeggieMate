97bfd84cb7e94881dfdad06032bb4b4ca6e01fc6
AnaModesto21 <77633954+AnaModesto21@users.noreply.github.com>
2022-05-09 18:56:00 +0100

    update

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,10 @@ cloudinary.config ({
     api_secret: process.env.CLOUDINARY_SECRET
 })
 
+var cors = require('cors')
+
+app.use(cors())
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser())
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,6 @@
 const app = require("./app");
 const cloudinary = require('cloudinary');
 const dotenv = require('dotenv');
-var cors = require('cors')
-
-app.use(cors())
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
 const PORT = process.env.PORT || 5005;
