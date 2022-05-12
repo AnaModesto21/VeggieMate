455f735d30ff995bed94b840f4ec4080caabc727
AnaModesto21 <77633954+AnaModesto21@users.noreply.github.com>
2022-05-12 18:52:01 +0100

    update

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -4,7 +4,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 // Process stripe payments
 exports.processPayment = catchAsyncErrors(async (req, res, next) => {
-
+    console.log('processPayment API CALLED');
     const paymentIntent = await stripe.paymentIntents.create({
         amount: req.body.amount,
         currency: 'eur',
@@ -21,7 +21,7 @@ exports.processPayment = catchAsyncErrors(async (req, res, next) => {
 
 // Send stripe API Key
 exports.sendStripApi = catchAsyncErrors(async (req, res, next) => {
-
+    console.log('sendStripApi API CALLED');
     res.status(200).json({
         stripeApiKey: process.env.STRIPE_API_KEY
     })
