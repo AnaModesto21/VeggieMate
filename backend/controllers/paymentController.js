403bba9273ef2460e59f4059cf2e13b9407ca765
AnaModesto21 <77633954+AnaModesto21@users.noreply.github.com>
2022-05-12 19:00:46 +0100

    update

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -1,35 +1,36 @@
-const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
+const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // Process stripe payments
 exports.processPayment = catchAsyncErrors(async (req, res, next) => {
-    console.log('processPayment API CALLED');
-    // const paymentIntent = await stripe.paymentIntents.create({
-    //     amount: req.body.amount,
-    //     currency: 'eur',
+  console.log("processPayment API CALLED");
+  // const paymentIntent = await stripe.paymentIntents.create({
+  //     amount: req.body.amount,
+  //     currency: 'eur',
 
-    //     metadata: { integration_check: 'accept_a_payment' }
-    // });
-    
+  //     metadata: { integration_check: 'accept_a_payment' }
+  // });
+  try {
     const paymentIntent = await stripe.paymentIntents.create({
-        amount: 2000,
-        currency: 'eur',
-        payment_method_types: ['card'],
-      });
+      amount: 2000,
+      currency: "eur",
+      payment_method_types: ["card"],
+    });
 
     res.status(200).json({
-        success: true,
-        client_secret: paymentIntent.client_secret
-    })
-
-})
+      success: true,
+      client_secret: paymentIntent.client_secret,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 // Send stripe API Key
 exports.sendStripApi = catchAsyncErrors(async (req, res, next) => {
-    console.log('sendStripApi API CALLED');
-    res.status(200).json({
-        stripeApiKey: process.env.STRIPE_API_KEY
-    })
-
-})
+  console.log("sendStripApi API CALLED");
+  res.status(200).json({
+    stripeApiKey: process.env.STRIPE_API_KEY,
+  });
+});
